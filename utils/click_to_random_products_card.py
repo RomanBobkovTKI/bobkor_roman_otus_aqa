3ceb3dd8c172389448269e362949143c4ada4f99
Roman Bobkov <bobkov.roman5@wildberries.work>
2026-02-08 14:49:06 +0300

    add test for product card

--- utils/click_to_random_products_card.py
+++ utils/click_to_random_products_card.py
@@ -1,5 +1,6 @@
 import random
 
+
 def click_to_random_elements(driver, search_func, selector):
     products_card = search_func(selector, driver)
     products_card[random.randint(0, len(products_card) - 1)].click()
