d99a3976a563d1aafa9ad98a36a8b424bd4f6f6b
Roman Bobkov <bobkov.roman5@wildberries.work>
2026-02-16 00:01:42 +0300

    refactoring test_clothes_page to page object model pattern

--- page_object_model/clothes_page.py
+++ page_object_model/clothes_page.py
@@ -4,10 +4,48 @@ from page_object_model.base_page import BasePage
 
 
 class ClothesPage(BasePage):
+    URL = "3-clothes"
+
     HEADER = (By.CSS_SELECTOR, "h1")
+    ALL_BREADCRUMBS_TEXT = (By.CSS_SELECTOR, ".breadcrumb span")
+    SUBCATEGORIES_HEADER = (By.CSS_SELECTOR, ".subcategory-heading")
+    ALL_SUBCATEGORIES = (By.CSS_SELECTOR, ".subcategory-name")
+    MEN_CATEGORIES_BUTTON = (
+        By.XPATH,
+        "//div[contains(@class, 'subcategory-image')]//a[contains(@href, '4-men')]",
+    )
+    WOMEN_CATEGORIES_LINK = (By.CSS_SELECTOR, "a[title='Women']")
 
     @property
     def header(self):
         header = self.wait_element(self.HEADER)
 
         return header
+
+    @property
+    def breadcrumbs_text_list(self):
+        breadcrumbs_text = self.wait_elements(self.ALL_BREADCRUMBS_TEXT)
+        breadcrumbs_list = [el.text.strip() for el in breadcrumbs_text]
+
+        return breadcrumbs_list
+
+    @property
+    def subcategories_header(self):
+        subcategories = self.wait_element(self.SUBCATEGORIES_HEADER)
+
+        return subcategories
+
+    @property
+    def all_subcategories_list(self):
+        all_subcategories = self.wait_elements(self.ALL_SUBCATEGORIES)
+        list_subcategories = [el.text.strip() for el in all_subcategories]
+
+        return list_subcategories
+
+    def click_to_men_categories_button(self):
+        button = self.wait_element(self.MEN_CATEGORIES_BUTTON)
+        button.click()
+
+    def click_to_women_categories_link(self):
+        link = self.wait_element(self.WOMEN_CATEGORIES_LINK)
+        link.click()
--- tests/test_clothes_page.py
+++ tests/test_clothes_page.py
@@ -1,34 +1,33 @@
 import pytest
-from selenium.webdriver.common.by import By
 
 from fixtures.driver import driver
 from fixtures.url import presta_shop_url
-from utils.wait_element import wait_element, wait_elements
+from page_object_model.base_page import BasePage
+from page_object_model.clothes_page import ClothesPage
+from page_object_model.elements.header import Header
+from page_object_model.main_page import MainPage
+from page_object_model.men_categories_page import MenCategories
+from page_object_model.women_categories_page import WomenCategories
 from utils.currency_symbol import extract_currency_symbol
 
 
 @pytest.mark.clothes_page
-@pytest.mark.parametrize("presta_shop_url", ["3-clothes"], indirect=True)
-def test_clothes_page(driver, presta_shop_url):
-    driver.get(presta_shop_url)
+@pytest.mark.parametrize("presta_shop_url", [ClothesPage.URL], indirect=True)
+def test_clothes_page(driver):
+    expected_header_text = "clothes"
+    actual_header_text = ClothesPage(driver).header.text.strip().lower()
 
-    header = wait_element("h1", driver)
-    header_text = header.text
-
-    assert "CLOTHES" in header_text, (
-        f"Неверный header, ожидалось {'CLOTHES'}, получили {header_text.text}"
+    assert expected_header_text in actual_header_text, (
+        f"Неверный header, ожидалось {expected_header_text}, получили {actual_header_text}"
     )
-    assert header.is_displayed(), f"Не отображается header"
+    assert ClothesPage(driver).header.is_displayed(), f"Не отображается header"
 
 
 @pytest.mark.clothes_page
-@pytest.mark.parametrize("presta_shop_url", ["3-clothes"], indirect=True)
-def test_breadcrumbs(driver, presta_shop_url):
-    driver.get(presta_shop_url)
-
-    breadcrumbs = wait_elements(".breadcrumb span", driver)
-    actual_texts = [el.text.strip() for el in breadcrumbs]
+@pytest.mark.parametrize("presta_shop_url", [ClothesPage.URL], indirect=True)
+def test_breadcrumbs(driver):
     expected_texts = ["Home", "Clothes"]
+    actual_texts = ClothesPage(driver).breadcrumbs_text_list
 
     assert actual_texts == expected_texts, (
         f"Неверные элементы в хлебных крошках, ожидалось {expected_texts}, получили {actual_texts}"
@@ -36,87 +35,75 @@ def test_breadcrumbs(driver, presta_shop_url):
 
 
 @pytest.mark.clothes_page
-@pytest.mark.parametrize("presta_shop_url", ["3-clothes"], indirect=True)
-def test_subcategories(driver, presta_shop_url):
-    driver.get(presta_shop_url)
+@pytest.mark.parametrize("presta_shop_url", [ClothesPage.URL], indirect=True)
+def test_subcategories(driver):
+    expected_header_text = "subcategories"
+    subcategories_text = ClothesPage(driver).subcategories_header.text.strip().lower()
 
-    subcategories = wait_element(".subcategory-heading", driver)
-    subcategories_text = subcategories.text
-
-    assert "Subcategories" == subcategories_text, f"Неверный текст у Subcategories"
-    assert subcategories.is_displayed(), f"Хедер Subcategories не отображается"
+    assert expected_header_text == subcategories_text, (
+        f"Неверный текст у подзаголовка, ожидалось: {expected_header_text}, получили {subcategories_text}"
+    )
+    assert ClothesPage(driver).subcategories_header.is_displayed(), (
+        f"Хедер Subcategories не отображается"
+    )
 
 
 @pytest.mark.clothes_page
-@pytest.mark.parametrize("presta_shop_url", ["3-clothes"], indirect=True)
-def test_subcategories_clothes(driver, presta_shop_url):
-    driver.get(presta_shop_url)
-
-    subcategories = wait_elements(".subcategory-name", driver)
-    actual_texts = [el.text.strip() for el in subcategories]
+@pytest.mark.parametrize("presta_shop_url", [ClothesPage.URL], indirect=True)
+def test_subcategories_clothes(driver):
     expected_texts = ["MEN", "WOMEN"]
+    actual_texts = ClothesPage(driver).all_subcategories_list
 
-    assert expected_texts == actual_texts
-
-
-@pytest.mark.clothes_page
-@pytest.mark.parametrize("presta_shop_url", ["3-clothes"], indirect=True)
-def test_click_on_men_categories(driver, presta_shop_url):
-    driver.get(presta_shop_url)
-
-    button = wait_element(
-        "//div[contains(@class, 'subcategory-image')]//a[contains(@href, '4-men')]",
-        driver,
-        by=By.XPATH,
-    )
-    button.click()
-
-    header = wait_element("h1", driver)
-    header_text = header.text
-
-    assert "MEN" == header_text, (
-        f"Неверный заголовок страницы, ожидалось: MEN, получили {header_text}"
-    )
-    assert "4-men" in driver.current_url, (
-        f"Неверный url после перехода, ожидалось содержание 4-men, получили {driver.current_url}"
+    assert expected_texts == actual_texts, (
+        f"Некорректные сабкатегории, ожидалось: {expected_texts}, получили {actual_texts}"
     )
 
 
 @pytest.mark.clothes_page
-@pytest.mark.parametrize("presta_shop_url", ["3-clothes"], indirect=True)
-def test_click_on_women_categories(driver, presta_shop_url):
-    driver.get(presta_shop_url)
+@pytest.mark.parametrize("presta_shop_url", [ClothesPage.URL], indirect=True)
+def test_click_on_men_categories(driver):
+    expected_header_text = "men"
+    expected_men_url = "4-men"
+    ClothesPage(driver).click_to_men_categories_button()
+    actual_header_text = MenCategories(driver).header.text.strip().lower()
 
-    button = wait_element("a[title='Women']", driver)
-    button.click()
-
-    header = wait_element("h1", driver)
-    header_text = header.text
-
-    assert "WOMEN" == header_text, (
-        f"Неверный заголовок страницы, ожидалось: WOMEN, получиди {header_text}"
+    assert expected_header_text == actual_header_text, (
+        f"Неверный заголовок страницы, ожидалось: {expected_header_text}, получили {actual_header_text}"
     )
-    assert "5-women" in driver.current_url, (
-        f"Неверный url после перехода, ожидалось содержание 5-women, получили {driver.current_url}"
+    assert expected_men_url in BasePage(driver).current_url, (
+        f"Неверный url после перехода, ожидалось содержание {expected_men_url}, получили {BasePage(driver).current_url}"
     )
 
 
 @pytest.mark.clothes_page
-@pytest.mark.parametrize("presta_shop_url", ["3-clothes"], indirect=True)
-def test_change_currencly(driver, presta_shop_url):
-    driver.get(presta_shop_url)
+@pytest.mark.parametrize("presta_shop_url", [ClothesPage.URL], indirect=True)
+def test_click_on_women_categories(driver):
+    expected_header_text = "women"
+    expected_women_url = "5-women"
+    ClothesPage(driver).click_to_women_categories_link()
+    actual_header_text = WomenCategories(driver).header.text.strip().lower()
 
-    random_price = wait_element(".price", driver).text
+    assert expected_header_text == actual_header_text, (
+        f"Неверный заголовок страницы, ожидалось: {expected_header_text}, получиди {actual_header_text}"
+    )
+    assert expected_women_url in BasePage(driver).current_url, (
+        f"Неверный url после перехода, ожидалось содержание 5-women, получили {BasePage(driver).current_url}"
+    )
+
+
+@pytest.mark.clothes_page
+@pytest.mark.parametrize("presta_shop_url", [ClothesPage.URL], indirect=True)
+def test_change_currency(driver, presta_shop_url):
+    expected_dollar_query = "?SubmitCurrency=1&id_currency=2"
+
+    random_price = MainPage(driver).currency_price_element.text
     currency_symbol_before = extract_currency_symbol(random_price)
 
-    change_button = wait_element(".hidden-sm-down.btn-unstyle", driver)
-    change_button.click()
+    Header(driver).click_to_change_currency_button()
+    Header(driver).click_to_dollar_value_in_currency_option()
 
-    dollar_button = wait_element("a[title='US Dollar']", driver)
-    dollar_button.click()
-
-    random_price = wait_element(".price", driver).text
+    random_price = MainPage(driver).currency_price_element.text
     currency_symbol_after = extract_currency_symbol(random_price)
 
     assert currency_symbol_before != currency_symbol_after
-    assert "?SubmitCurrency=1&id_currency=2" in driver.current_url
+    assert expected_dollar_query in BasePage(driver).current_url
